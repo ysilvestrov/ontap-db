554c5392d21d7eefc9e27a0850830ce52e3a5b89
Yuriy Silvestrov <yuriy@silvestrov.com>
2018-09-19 00:19:08 +0300

    Fixed login

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,51 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { ReactiveFormsModule } from '@angular/forms';
+import {JwtHelperService, JwtModule} from '@auth0/angular-jwt';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import {RouterModule, Routes} from '@angular/router';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import {AuthGuard} from './auth.guard';
+import {root} from 'rxjs/internal-compatibility';
+import {tokenGetter} from './auth.service';
+import {HttpClientModule} from '@angular/common/http';
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
+
+const appRoutes: Routes = [
+  { path: 'login', component: LoginComponent},
+  { path: 'dashboard', component: DashboardComponent, canActivate: [AuthGuard]},
+  {
+    path: '',
+    redirectTo: '/dashboard',
+    pathMatch: 'full',
+    canActivate: [AuthGuard]
+  },
+  { path: '**', component: PageNotFoundComponent }
+];
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    PageNotFoundComponent,
+    DashboardComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    ReactiveFormsModule,
+    RouterModule.forRoot(appRoutes),
+    HttpClientModule,
+    JwtModule.forRoot({
+      config: {
+        tokenGetter: tokenGetter,
+        whitelistedDomains: ['localhost:3001'],
+        blacklistedRoutes: ['localhost:3001/auth/']
+      }
+    }),
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
